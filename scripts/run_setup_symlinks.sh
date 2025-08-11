b8411be668b4db5e3b18e6cff036d3d4cc36a36e
clintko <clint00724@gmail.com>
2025-08-11 11:15:20 -0400

    setup link for data, results, log folder and add them to gitignore file

---
+++ scripts/run_setup_symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s /hpc/group/igvf/kk319/work/Proj_IGVF_BlueSTARR/results results
+ln -s /hpc/group/igvf/kk319/work/Proj_IGVF_BlueSTARR/log log
+ln -s /hpc/group/igvf/kk319/work/Proj_IGVF_BlueSTARR/data data
